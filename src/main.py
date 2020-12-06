9b82cb09f6a160176b9860e3426e4330a7dc19c6
Falguni Das <dasshuvo7@gmail.com>
2020-12-06 08:15:39 +0600

    Update main.py.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
+from saved_models import saved_model_dir
 from src.environment.impl.cartpole_environment import CartPoleEnvironment
 from src.policy.reinforce import Reinforce
 from src.utils.summary_writer import SummaryWriter
 
 environment = CartPoleEnvironment()
 summary_writer = SummaryWriter()
+save_model_path = saved_model_dir
 
-agent_policy = Reinforce(environment, summary_writer)
+agent_policy = Reinforce(environment, summary_writer, save_model_path=save_model_path)
 
 agent_policy.learn_optimal_policy()
 
