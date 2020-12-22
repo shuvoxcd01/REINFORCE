50738780458f08f0c8757d96e6481ad663a067e7
Falguni Das <dasshuvo7@gmail.com>
2020-12-22 20:29:08 +0600

    Let CartPoleEnvironment use full number of steps given by gym.

--- src/environment/impl/cartpole_environment.py
+++ src/environment/impl/cartpole_environment.py
@@ -6,22 +6,14 @@ from src.environment.base_environment import BaseEnvironment
 class CartPoleEnvironment(BaseEnvironment):
     def __init__(self):
         self.env = gym.make("CartPole-v0")
-        self.max_num_step = 20
-        self.step_count = 0
 
     def reset(self):
-        self.step_count = 0
         return self.env.reset()
 
     def step(self, action):
         observation, reward, done, info = self.env.step(action)
 
-        self.step_count += 1
-
-        if self.step_count >= self.max_num_step:
-            done = True
-
-        reward = reward if not done else -1
+        reward = reward if not done else -10
 
         return observation, reward, done, info
 
